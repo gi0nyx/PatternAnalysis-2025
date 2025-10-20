13ef67c7885bbac37fecd9c70d77b16ad850acd4
gi0nyx <shiroganek34@gmail.com>
2025-10-20 15:52:26 +1000

    adds testing code to train.py

--- recognition/alzheimer_classification_s4863083/train.py
+++ recognition/alzheimer_classification_s4863083/train.py
@@ -6,34 +6,41 @@ from sklearn.metrics import accuracy_score
 from scipy.stats import mode
 import torch
 import torch.nn as nn
+import cv2
 from torch.utils.data import DataLoader
 from torch.cuda.amp import autocast, GradScaler
 from tqdm import tqdm
 from modules import ConvNext, create_base
 
 from albumentations import (
-    Compose, RandomResizedCrop, Normalize, Resize
+    Compose, RandomResizedCrop, Normalize, Resize, GaussNoise, RandomBrightnessContrast, ShiftScaleRotate, HorizontalFlip
 )
 from albumentations.pytorch import ToTensorV2
 from dataset import TrainDataset
 
 class CFG:
     batch_size = 16
-    epochs = 25
-    lr = 1e-4 
+    epochs = 20
+    lr = 1e-3 
     img_size = 224
     n_splits = 5
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+def set_seed(seed=42):
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
 train_transform = Compose([
         RandomResizedCrop(size=(CFG.img_size, CFG.img_size), scale=(0.8, 1.0)),
-        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ToTensorV2(),
     ])
 
 val_transform = Compose([
         Resize(CFG.img_size, CFG.img_size, always_apply=True),
-        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ToTensorV2(),
     ])
 
@@ -58,9 +65,12 @@ def calculate_patient_level_accuracy(df_subset, preds):
 
 def main():
 
+    set_seed(42)
+
     df = pd.read_csv('train_df.csv')
 
     sgkf = StratifiedGroupKFold(n_splits=CFG.n_splits, shuffle=True, random_state=42)
+
     train_idx, val_idx = list(sgkf.split(df, df['label'], df['patient_id']))[0]
 
     train_df = df.iloc[train_idx].reset_index(drop=True)
@@ -71,8 +81,6 @@ def main():
 
     train_loader = DataLoader(train_dataset, batch_size=CFG.batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = DataLoader(val_dataset, batch_size=CFG.batch_size, shuffle=False, num_workers=4, pin_memory=True)
-
-    model_name = "convnextv2_tiny" 
     
     model = create_base()
     model = model.to(CFG.device)
@@ -112,7 +120,7 @@ def main():
         train_slice_acc = accuracy_score(train_epoch_labels, train_epoch_preds)
         train_patient_acc = calculate_patient_level_accuracy(train_df, train_epoch_preds)
 
-       
+    
         model.eval()
         val_loss = 0.0
         val_epoch_preds = []
@@ -136,8 +144,47 @@ def main():
         print(f"  Train -> Loss: {train_loss/len(train_loader):.4f} | Slice Acc: {train_slice_acc:.4f}")
         print(f"  Valid -> Loss: {val_loss/len(val_loader):.4f} | Slice Acc: {val_slice_acc:.4f} | Patient Acc: {val_patient_acc:.4f}\n")
 
-    torch.save(model.state_dict(), "convnext_base.pth")
+    torch.save(model.state_dict(), f"convnext_base_best.pth")
     print("Training complete, model saved.")
+
+    print('\n-----\n')
+
+    print("Doing Test")
+
+    device = CFG.device
+    model_path = "convnextv2_base.pth"
+    test_df = pd.read_csv('test_df.csv')
+
+
+    model = create_base()
+    model.load_state_dict(torch.load(f"convnext_base_best.pth", map_location=device))
+    model.to(device)
+    model.eval()
+
+    test_dataset = TrainDataset(test_df, transform=val_transform)
+    test_loader = DataLoader(test_dataset, batch_size=CFG.batch_size * 2, shuffle=False)
+
+    test_preds = []
+    test_labels = []
+
+    with torch.no_grad():
+        for images, labels in tqdm(test_loader, desc="Getting Slice Predictions"):
+            images = images.to(device)
+            outputs = model(images)
+            preds = outputs.argmax(dim=1).cpu().numpy()
+            test_preds.extend(preds)
+            test_labels.extend(labels)
+
+    test_slice_acc = accuracy_score(test_labels, test_preds)
+    test_patient_acc = calculate_patient_level_accuracy(test_df, test_preds)
+
+    print(f"Slice-Level Accuracy: {test_slice_acc:.4f}")
+    print(f"Patient-Level Accuracy (using Majority Vote): {test_patient_acc:.4f}")
+
+    test_df['pred'] = test_preds
+    test_df.to_csv('preds.csv', index=False)
+
+    print("Saved Prediction CSV")
 
 if __name__ == "__main__":
     main()
